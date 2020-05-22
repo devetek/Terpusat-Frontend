f2787e9d68a2ee956e77c45e08c2ee899408b77c
Nedya Prakasa <nedya.prakasa@tokopedia.com>
2020-05-22 11:16:52 +0700

    fix(repo): remove unused component React
    remove unused component React Route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Redirect, Route } from "react-router-dom";
+import { Switch, Redirect } from "react-router-dom";
 import HomeComponent from "routes/HomePage";
 import { Main as MainLayout } from "Layout";
 import RouteWithLayout from "components/RouteWithLayout";
