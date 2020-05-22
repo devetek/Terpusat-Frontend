5f584f16f6ed1ae0dfccaa7d4c5ae0ef8952f541
Nedya Prakasa <nedya.prakasa@tokopedia.com>
2020-05-22 11:15:26 +0700

    fix(repo): remove unused code
    remove unused code to prevent error on build for typescript linting

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,21 +6,21 @@ import RouteWithLayout from "components/RouteWithLayout";
 // import Header from "components/Header";
 // import Footer from "components/Footer";
 
-const sections = [
-  { title: "Store", url: "https://www.tokopedia.com/terpusat" },
-  { title: "Style", url: "https://www.instagram.com/ulooks_id" },
-  { title: "Techno", url: "https://devetek.com" },
-  { title: "Invest", url: "https://www.facebook.com/TerpusatInvestment" },
-  {
-    title: "Media",
-    url: "https://www.youtube.com/channel/UCi9ueSOWcbgLfb0sCW_BWbA",
-  },
-  {
-    title: "Health",
-    url: "https://www.facebook.com/InformasiKesehatanTerpusat",
-  },
-  { title: "Travel", url: "https://mypermatawisata.com/" },
-];
+// const sections = [
+//   { title: "Store", url: "https://www.tokopedia.com/terpusat" },
+//   { title: "Style", url: "https://www.instagram.com/ulooks_id" },
+//   { title: "Techno", url: "https://devetek.com" },
+//   { title: "Invest", url: "https://www.facebook.com/TerpusatInvestment" },
+//   {
+//     title: "Media",
+//     url: "https://www.youtube.com/channel/UCi9ueSOWcbgLfb0sCW_BWbA",
+//   },
+//   {
+//     title: "Health",
+//     url: "https://www.facebook.com/InformasiKesehatanTerpusat",
+//   },
+//   { title: "Travel", url: "https://mypermatawisata.com/" },
+// ];
 
 const Routes: React.FC = () => {
   return (
