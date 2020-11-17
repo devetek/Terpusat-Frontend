f2611aa88c239e81f5b368e01e22969494d7d684
Amrih Prakasa <nedya.prakasa@tokopedia.com>
2020-11-17 19:52:28 +0700

    feat(ui): dashboard page dummy design v2 (#45)
    dashboard page dummy design v2

--- src/routes/DashboardPage/components/index.tsx
+++ src/routes/DashboardPage/components/index.tsx
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import { RightSidebar } from "components/Container";
-import Header from "./Header";
-import Toolbar from "./Toolbar";
+// import Header from "./Header";
+// import Toolbar from "./Toolbar";
 import Content from "./Content";
 import { PopularWidget } from "./Sidebar";
 
 function DashboardPage() {
-  const [activeContent, setActiveContent] = useState(0);
+  // const [activeContent, setActiveContent] = useState(0);
 
   return (
     <RightSidebar
-      showHeader
-      headerContent={<Header />}
-      showToolbar
-      toolbarContent={<Toolbar activeContent={activeContent} setActiveContent={setActiveContent} />}
+      // showHeader
+      // headerContent={<Header />}
+      // showToolbar
+      // toolbarContent={<Toolbar activeContent={activeContent} setActiveContent={setActiveContent} />}
       showBody
-      bodyContent={<Content activeContent={activeContent} />}
+      bodyContent={<Content activeContent={0} />}
       showSidebar
       sidebarContent={
         <>
